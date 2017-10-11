f13718eca8d47ee9ac5e028431211f9ea55eee4d
Titipat <ttpsvb@gmail.com>
2017-10-11 11:42:32 +0700

    Add migration scripts

---
+++ database/migrations/2017_10_11_040654_create_projects_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('course_id', 6);
+            $table->string('section_no', 3);
+            $table->integer('semester');
+            $table->integer('year');
+            $table->string('adviser_id', 3);
+            $table->float('midterm_score')->nullable();
+            $table->float('final_score')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+}
